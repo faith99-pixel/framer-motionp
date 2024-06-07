4bde31a3993d7423b802da61ce717b6df88fb76e
Zoe Zebedee <zoezebedee1@gmail.com>
2024-06-07 02:19:05 +0100

    Ui updates with Framer motion

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,73 @@
-import Image from "next/image";
-import ButtonFeedback from "./components/animatedbutton/ButtonFeedback";
+"use client"
+
+import Button from "@/app/components/animatedbutton/Button";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
 
 export default function Home() {
+  const [showForm, setShowForm] = useState(false);
+
   return (
-    <main className="flex flex-col p-14">
-      <ButtonFeedback />
-    </main>
+    <div className="flex items-center justify-center h-dvh w-full bg-[#131519]">
+      <AnimatePresence mode="wait">
+        {showForm ? (
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            transition={{ duration: 0.3 }}
+            key={"form"}
+          >
+            <div className="px-6 py-8 w-[351px] bg-[#202831] rounded-3xl text-white">
+              <div className="p-2.5 bg-[#272e3a] rounded-full w-fit">
+                <img
+                  src="/images/Vector.svg"
+                  alt=""
+                  className=""
+                  width={20}
+                  height={20}
+                />
+              </div>
+              <h3 className="font-semibold text-lg mt-6">How did we do?</h3>
+
+              <p className="text-sm text-white text-opacity-80 mt-2">
+                Please let us know we did with your support request. All
+                feedback is appreciated to help us improve our service!{" "}
+              </p>
+              <div className="flex justify-between mt-8 ">
+                {[1, 2, 3, 4, 5].map((item, idx) => (
+                  <div
+                    key={idx}
+                    className="flex items-center justify-center rounded-full bg-[#272e3a] hover:bg-[#fa7710] p-2 w-[44.75px] h-[44.75px] hover:-translate-y-2 ease-linear duration-75 transition cursor-pointer text-white/50 hover:text-white/100"
+                  >
+                    <span className="text-sm">{item}</span>
+                  </div>
+                ))}
+              </div>
+
+              <Button
+                title="Submit"
+                onClick={() => setShowForm(false)}
+                className="mt-8 max-w-[287px] mx-auto uppercase text-white"
+              />
+            </div>
+          </motion.div>
+        ) : (
+          <motion.div
+            initial={{ opacity: 1, y: -5 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 5 }}
+            transition={{ duration: 0.2 }}
+            key={"button"}
+          >
+            <Button
+              title="Submit feedback"
+              onClick={() => setShowForm(true)}
+              className=" max-w-[287px] mx-auto uppercase text-white"
+            />
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
   );
 }
